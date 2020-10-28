ab45f53c29accfc0326b36c4f1fc01277bbec7dd
EngineerBeginner <spright1038@gmail.com>
2020-10-28 18:02:23 +0900

    新規登録およびログインに関するビューファイルの配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, unique: true
+  validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
   validates :last_name, :first_name, presence: true
   validates :last_name_kana, :first_name_kana, presence: true
