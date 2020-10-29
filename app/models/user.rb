8c8828bdbbef298f73d5a6345b1cd6e8caa73a77
EngineerBeginner <spright1038@gmail.com>
2020-10-29 16:02:30 +0900

    【修正１】バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
-  NAME_KANA_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/.freeze
+  NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
 
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX }
   validates :password, presence: true, length: { minimum: 6}, format: { with: PASSWORD_REGEX, message: '6文字以上半角英数字で入力してください'}
   validates :last_name, :first_name, presence: true, format: { with: NAME_REGEX, message: '全角文字を使用してください' }
-  validates :last_name_kana, :first_name_kana, presence: true, format: { with: NAME_KANA_REGEX, message: 'カタカナを使用してください' }
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: NAME_KANA_REGEX, message: '全角カタカナを使用してください' }
   validates :birth_date, presence: true
 end
